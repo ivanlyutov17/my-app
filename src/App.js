76b930acef96b161fcc6e1cc86a014d87a984b44
ivanlyutov17 <lyutov17@yandex.ru>
2021-10-28 09:13:15 +0200

    Lesson-3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { useEffect } from 'react';
 import { useState } from 'react/cjs/react.development';
 import './App.css';
 import MessageList from './MessageList';
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import Chats from './Chats.js'
+import getId from './ChatsArray';
 
 
 function App(props) {
@@ -11,14 +15,14 @@ function App(props) {
   const getText = (e) => setText(e.target.value);
   const btnClick = () => {
     setMessageList([...messageList, {
-      author: author, text: text,
+      author: author, text: text, key: getId(),
     }]);
     console.log(messageList.key);
   }
   useEffect(() => {
     const timer = setTimeout(() => {
       if ((messageList.length !== 0) && (messageList[messageList.length - 1].author !== 'Bot')) {
-        setMessageList([...messageList, { author: 'Bot', text: 'Бот ответил' }]);
+        setMessageList([...messageList, { author: 'Bot', text: 'Бот ответил', key: getId(), }]);
         console.log(messageList)
       }
     }, 5000);
@@ -28,11 +32,17 @@ function App(props) {
   }, [messageList])
   return (
     <div className="App">
-      <MessageList messageList={messageList} />
-
-      <input value={text} onChange={getText} />
-      <button onClick={btnClick}>Check</button>
-
+      <div className="flexed">
+        <div className="chats">
+          <h3>Ваши чаты</h3>
+          <Chats />
+        </div>
+        <div className="buttons">
+          <MessageList messageList={messageList} />
+          <TextField className="inputMessage" autoFocus id="outlined-basic" label="Введите сообщение" variant="outlined" value={text} onChange={getText} />
+          <Button className="sendButton" variant="outlined" onClick={btnClick}>Отправить</Button>
+        </div>
+      </div>
     </div>)
 
 }
--- src/MessageList.js
+++ src/MessageList.js
@@ -1,12 +1,14 @@
 import './MessageList.css'
 function MessageList({ messageList }) {
-
-    return (<div>
-        {messageList.map((message) => (<div>
-            <p className='author'>{message.author}</p>
-            <p className='message'>{message.text}</p>
-        </div>))}
-    </div>)
+    // key = `${message.key}`
+    return (<div className="messagesArea">
+        {
+            messageList.map((message,) => (<div className="message" key={message.key} >
+                <p className='author'>{message.author}</p>
+                <p className='messageText'>{message.text}</p>
+            </div>))
+        }
+    </div >)
 };
 
 export default MessageList;
